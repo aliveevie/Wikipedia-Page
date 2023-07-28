2eb81f49ecdf29e4b31859363979abe9629cbe8a
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-07-28 10:02:08 +0100

    fix: modified scr/App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState} from 'react';
+
+
+
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  function handleSearch() {
+    axios.get(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchTerm}&origin=*&format=json`)
+    .then(response => {
+     // setLoadingStatus(false);
+      setSearchResults(response.data.query.search, searchTerm);
+      
+    }).catch(error => {
+     // setLoadingStatus(false);
+     console.log(error)
+      console.log('Unable to load Wikipedia search results at this time.');
+    });
+  }
+
+  const truncateDescription = (description, maxLines = 2) => {
+    const lines = description.split('\n');
+    return lines.slice(0, maxLines).join('\n');
+  };
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='input-container'>
+      <input type="text" value={searchTerm} onChange={handleChange} />
+      <button onClick={handleSearch}>Search</button>
+      </div>
+      <ul>
+      {searchResults.map((result) => (
+          <li key={result.pageid}>
+            <a href={`https://en.wikipedia.org/?curid=${result.pageid}`} target="_blank" rel="noopener noreferrer">
+              <h3>{result.title}</h3>
+              <div dangerouslySetInnerHTML={{ __html: truncateDescription(result.snippet) }} />
+            </a>
+          </li>
+        ))}
+      </ul>
       </header>
     </div>
   );
